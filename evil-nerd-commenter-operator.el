ac03a92ae6581a7df503ac77d3b1280d4b51930a
Chen Bin <chenbin.sh@gmail.com>
2014-10-31 21:16:51 +1100

    updated README v1.5.10

--- evil-nerd-commenter-operator.el
+++ evil-nerd-commenter-operator.el
@@ -4,7 +4,7 @@
 
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/evil-nerd-commenter
-;; Version: 1.5.9
+;; Version: 1.5.10
 ;; Keywords: commenter vim line evil
 ;;
 ;; This file is not part of GNU Emacs.
