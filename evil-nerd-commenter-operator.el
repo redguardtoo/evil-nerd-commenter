2401d871adff3436ab8c21047719a47d47232e6d
Chen Bin <chenbin.sh@gmail.com>
2020-06-02 20:02:47 +1000

    use GPL version 3

--- evil-nerd-commenter-operator.el
+++ evil-nerd-commenter-operator.el
@@ -1,4 +1,4 @@
-;;; evil-nerd-commenter-operator.el --- Provides an evil operator for evil-nerd-commenter
+;;; evil-nerd-commenter-operator.el --- evil operator for this program
 
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 
@@ -8,7 +8,7 @@
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
 ;; This program is distributed in the hope that it will be useful,
