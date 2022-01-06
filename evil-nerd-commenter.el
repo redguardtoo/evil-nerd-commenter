42ba1a473b4f1df061baddd2f8b812a2f35e366e
Chen Bin <chenbin.sh@gmail.com>
2022-01-06 23:24:05 +1100

    nongnu-elpa badge

--- evil-nerd-commenter.el
+++ evil-nerd-commenter.el
@@ -3,7 +3,7 @@
 ;; Author: Chen Bin <chenbin DOT sh AT gmail.com>
 
 ;; URL: http://github.com/redguardtoo/evil-nerd-commenter
-;; Version: 3.5.5
+;; Version: 3.5.6
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: convenience evil
 ;;
@@ -782,7 +782,7 @@ Then we operate the expanded region.  NUM is ignored."
 (defun evilnc-version ()
   "The version number."
   (interactive)
-  (message "3.5.5"))
+  (message "3.5.6"))
 
 (defvar evil-normal-state-map)
 (defvar evil-visual-state-map)
--- pkg.sh
+++ pkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=evil-nerd-commenter
-version=3.5.5
+version=3.5.6
 pkg=$name-$version
 mkdir $pkg
 cp *.el $pkg
